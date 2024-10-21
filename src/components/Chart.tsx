2fa0a884324a123b555d0f69795d0f573571a896
Keivon <Keivon.991@gmail.com>
2024-10-21 16:43:29 +0100

    refactor: simplify chart component by moving Series and Options interfaces to types file

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,69 +1,10 @@
 
 import { useState, useEffect } from 'react';
 import LineChart from "react-apexcharts";
-import {ChartProps} from '@/types/types';
+import { ChartProps, Options, Series } from '@/types/types';
 
-const Chart: React.FC<ChartProps> = ( {stationsList} ) => {
+const Chart: React.FC<ChartProps> = ({ stationsList }) => {
 
-
-    interface Series {
-        name: string;
-        data: { x: string; y: number; name: string }[];
-    }
-
-    interface Options {
-        chart: {
-            id: string;
-            toolbar: {
-                show: boolean;
-            };
-        };
-        xaxis: {
-            type: "category";
-            labels: {
-                show: boolean;
-            };
-            axisBorder: {
-                show: boolean;
-            };
-            axisTicks: {
-                show: boolean;
-            };
-        };
-        yaxis: {
-            show: boolean;
-        };
-        grid: {
-            show: boolean;
-        };
-        markers: {
-            size: number;
-            hover: {
-                sizeOffset: number;
-            };
-        };
-        dataLabels: {
-            enabled: boolean;
-            formatter: (val: number, opts: any) => string;
-            style: {
-                fontSize: string;
-                colors: string[];
-            };
-            background: {
-                enabled: boolean;
-                borderRadius: number;
-            };
-        };
-        stroke: {
-            curve: "smooth";
-        };
-        tooltip: {
-            enabled: boolean;
-        };
-        legend: {
-            show: boolean;
-        };
-    }
 
     const [series, setSeries] = useState<Series[]>([]);
     const [options, setOptions] = useState<Options>();
@@ -72,73 +13,79 @@ const Chart: React.FC<ChartProps> = ( {stationsList} ) => {
     useEffect(() => {
         const options = {
             chart: {
-              id: "line-chart",
-              toolbar: {
-                show: false, // Hide the toolbar (zoom, pan, etc.)
-              },
+                id: "line-chart",
+                toolbar: {
+                    show: false, // Hide the toolbar (zoom, pan, etc.)
+                },
             },
             xaxis: {
-              type: "category" as "category",
-              labels: {
-                show: false, // Hide x-axis labels
-              },
-              axisBorder: {
-                show: false, // Hide the x-axis border line
-              },
-              axisTicks: {
-                show: false, // Hide x-axis ticks
-              },
+                type: "category" as "category",
+                labels: {
+                    show: false, // Hide x-axis labels
+                },
+                axisBorder: {
+                    show: false, // Hide the x-axis border line
+                },
+                axisTicks: {
+                    show: false, // Hide x-axis ticks
+                },
             },
             yaxis: {
-              show: false, // Hide the y-axis
+                show: false, // Hide the y-axis
             },
             grid: {
-              show: false, // Remove grid lines
+                show: false, // Remove grid lines
+                padding: {
+                    top: 0,
+                    right: 110,
+                    bottom: 0,
+                    left: 110,
+                  },
             },
             markers: {
-              size: 9,
-              hover: {
-                sizeOffset: 3,
-              },
+                size: 9,
+                hover: {
+                    sizeOffset: 3,
+                },
             },
             dataLabels: {
-              enabled: true,
-              formatter: function (val:any, opts: any) {
-                // Get the marker name from the data series
-                return opts.w.config.series[opts.seriesIndex].data[opts.dataPointIndex].name;
-              },
-              style: {
-                fontSize: "12px",
-                colors: ["#000"],
-              },
-              background: {
                 enabled: true,
-                borderRadius: 2,
-              },
+                formatter: function (val: any, opts: any) {
+                    // Get the marker name from the data series
+                    return opts.w.config.series[opts.seriesIndex].data[opts.dataPointIndex].name;
+                },
+                style: {
+                    fontSize: "12px",
+                    colors: ["#000"],
+                },
+                background: {
+                    enabled: true,
+                    borderRadius: 2,
+                },
             },
             stroke: {
-              curve: "smooth" as "smooth",
+                curve: "smooth" as "smooth",
             },
             tooltip: {
-              enabled: false, // Disable tooltips
+                enabled: false, // Disable tooltips
             },
             legend: {
-              show: false, // Hide legend
+                show: false, // Hide legend
             },
-          };
-          setOptions(options);
+        };
+        setOptions(options);
 
-            const series = [
-                {
+        const series = [
+            {
                 name: "Line 1",
                 data: stationsList?.map((station, index) => ({
                     x: index.toString(),
-                    y: Math.floor(index  * 10),
+                    y: Math.floor(index * 10),
                     name: station,
                 })) || [],
-                },
-            ];
-            setSeries(series);
+            },
+        ];
+        setSeries(series);
     }, []);
 
 
@@ -146,10 +93,10 @@ const Chart: React.FC<ChartProps> = ( {stationsList} ) => {
 
 
     return (
-        <div className='h-[68vh] w-[58vw]'>{
+        <div>{
             options && series &&
-        <LineChart options={options} series={series} type="line"  />
-}
+            <LineChart options={options} series={series} type="line" />
+        }
         </div>
     );
 };
--- src/types/types.tsx
+++ src/types/types.tsx
@@ -2,12 +2,89 @@
 export interface CheapTflSvgProps {
     width?: string;
     height?: string;
-  }
+}
 
-  export interface ChartProps {
+export interface ChartProps {
     stationsList?: string[];
-  }
+}
 
- export interface FareData {
+export interface FareData {
     [key: string]: string;
-  }
+}
+
+export interface Series {
+    name: string;
+    data: { x: string; y: number; name: string }[];
+}
+
+export interface Options {
+    chart: {
+        id: string;
+        toolbar: {
+            show: boolean;
+        };
+        padding?: {
+            top?: number;
+            right?: number;
+            bottom?: number;
+            left?: number;
+        };
+    };
+    xaxis: {
+        type: "category";
+        labels: {
+            show: boolean;
+        };
+        axisBorder: {
+            show: boolean;
+        };
+        axisTicks: {
+            show: boolean;
+        };
+    };
+    yaxis: {
+        show: boolean;
+    };
+    grid: {
+        show: boolean;
+        padding?: {
+            top?: number;
+            right?: number;
+            bottom?: number;
+            left?: number;
+        };
+    };
+    padding?: {
+        top?: number;
+        right?: number;
+        bottom?: number;
+        left?: number;
+    };
+    markers: {
+        size: number;
+        hover: {
+            sizeOffset: number;
+        };
+    };
+    dataLabels: {
+        enabled: boolean;
+        formatter: (val: number, opts: any) => string;
+        style: {
+            fontSize: string;
+            colors: string[];
+        };
+        background: {
+            enabled: boolean;
+            borderRadius: number;
+        };
+    };
+    stroke: {
+        curve: "smooth";
+    };
+    tooltip: {
+        enabled: boolean;
+    };
+    legend: {
+        show: boolean;
+    };
+}
